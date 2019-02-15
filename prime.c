e398c22cbb8f9ca7d2a81e8f4dbffb82a640623a
sridharN03 <47448873+sridharN03@users.noreply.github.com>
2019-02-15 23:13:59 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+   int i, n, p = 0;
+   scanf("%d", &n);
+   for(i=1; i<=n; i++)
+   {
+      if(n%i==0)
+      {
+         p++;
+      }
+   }
+   if(p==2)
+   {
+      printf("Yes",n);
+   }
+   else
+   {
+      printf(" no",n);
+   }
+}
